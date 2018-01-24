12467430e96eec67dc53dfff0f687bb657314bf5
yamada46 <yamadg@gmail.com>
2018-01-23 16:25:35 -0800

    hw2.py still need figure out tax

--- hw2.py
+++ hw2.py
@@ -21,42 +21,40 @@ Assign the car a unique ID composed of the last four letters of the purchaser's 
 Print out the information to the screen, with the same line breaks as shown in the example below
 Make sure your script runs and submit it to canvas as a .py file. 
 '''
-'''
+
 # Get the purchaser name
 purchaser_name = input("What is your first, middle and last name? Please separate your names with commas. \n")
 print(purchaser_name)
 
 fullname = purchaser_name.split(",")
-print(fullname)
+print(fullname[2])
 
 # Get purchaser address
 purchaser_address = input("Enter your address \n")
 print(purchaser_address)
-'''
+
 # Get purchaser phone number
 purchaser_phone = input("Enter your phone number, including area code xxx-xxx-xxxx \n")
 print(purchaser_phone)
 
 phone_number = purchaser_phone.split("-")
-print(phone_number[3])
+print(phone_number[2])
 
 # Enter the type of car purchased
 type_of_car = "Subaru CrossTrek"
 print(type_of_car)
 
-
-'''
-
 # Enter price of car purchase
-base_price = input("Base Price = (exact price including cents, no commas) " )
-#dollars, cents = base_price.split(".")
-print(base_price)
+base_price = input("Enter Base Price =  \n$ " )
+base_decimal = float(base_price)
+print(base_decimal)
 
-# see if there are 2 decimal places
 
 # Calculate sales tax
-sales_tax_rate = input("What is Seattle car sales tax rate? \n")
+sales_tax_rate = input("Enter Seattle car sales tax rate? \n")
 print(sales_tax_rate)
+
+'''
 if sales_tax_rate == sales_tax_rate + "%"):
     sales_tax = float((sales_tax_rate - "%")/100)
 
@@ -65,4 +63,12 @@ else
     
 
 print(sales_tax)
+
 '''
+# Enter the dealer's prep fee
+prep_fee = input("Enter the dealer's preparation fee: \n$ ")
+
+purchaserID = (fullname[2],"_"phone_number[2])
+
+
+
